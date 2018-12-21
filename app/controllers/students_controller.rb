fb3b4f05acb6aa9b4d2cea9a2d570a004102d906
Mika88 <mikaspivak@gmail.com>
2018-12-21 21:45:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,16 +5,20 @@ class StudentsController < ApplicationController
 
   def new
   end
-  
+
   def create
     @student = Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
-   
+
   def update
     @student = Student.find(params[:id])
     @student.update(student_params(:student))
     redirect_to student_path(@student)
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
