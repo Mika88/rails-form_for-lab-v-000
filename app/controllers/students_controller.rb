e8d0749398fd50207ffaac32644a424ceb33928d
Mika88 <mikaspivak@gmail.com>
2018-12-21 21:44:48 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,20 @@
 class StudentsController < ApplicationController
+  def show
+    @student = Student.find(params[:id])
+  end
 
+  def new
+  end
+  
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+   
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:student))
+    redirect_to student_path(@student)
+  end
 end
